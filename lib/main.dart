b8d9f34af6b784192131bb2f0e07411255c99347
Sandeep DK <sandeepsunil01@gmail.com>
2023-08-24 12:52:49 +0530

    Provider not found error overview

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:state_management/models/cat.dart';
 import 'package:state_management/models/dog.dart';
-
-import 'provider_overview/provider_overview_08.dart';
+import 'provider_overview/provider_overview_09.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,37 +13,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider<Dog>(
-            create: (context) =>
-                Dog(name: "Dog-08", breed: "Breed-08", age: 3)),
-        FutureProvider<int>(
-          initialData: 0,
-          create: (context) {
-            final int dogAge = context.read<Dog>().age;
-            final cats = Cats(age: dogAge);
-            return cats.getCatsAge();
-          },
-        ),
-        StreamProvider<String>(
-          initialData: 'Bark 0 times',
-          create: (context) {
-            final int dogAge = context.read<Dog>().age;
-            final cats = Cats(age: dogAge * 2);
-            return cats.bark();
-          },
-        ),
-      ],
-      child: MaterialApp(
-        title: 'State Management with Provider',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
-        ),
-        home: const ProviderOverview08(),
+    return MaterialApp(
+      title: 'State Management with Provider',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
       ),
+      home: const ProviderOverview09(),
     );
   }
 }
---
+++ lib/provider_overview/provider_overview_09.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+// There may be times when we get Provider Not Found
+// There may be 3 possinle solutions
+// 1. Add the Providet up the widget tree,
+// 2. Use Builder instead of child
+// 3. Use Consumer instead of direct builder
+
+class ProviderOverview09 extends StatefulWidget {
+  const ProviderOverview09({super.key});
+
+  @override
+  State<ProviderOverview09> createState() => _ProviderOverview09State();
+}
+
+class _ProviderOverview09State extends State<ProviderOverview09> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar:
+          AppBar(title: const Text("Consumer Error - (Not Found Provider)")),
+      body: ChangeNotifierProvider<Foo>(
+        create: (_) => Foo(),
+        child: Consumer<Foo>(
+            builder: (BuildContext context, Foo foo, Widget? child) {
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  foo.value,
+                  style: const TextStyle(fontSize: 40),
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    foo.changeValue();
+                  },
+                  child: const Text(
+                    "Change Value",
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ),
+              ],
+            ),
+          );
+        }),
+      ),
+    );
+  }
+}
+
+class Foo with ChangeNotifier {
+  String value = "Foo";
+
+  void changeValue() {
+    value = value == "Foo" ? "Bar" : "Foo";
+    notifyListeners();
+  }
+}
